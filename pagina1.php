522f20bd1b55f1e24005d6847d102dcc2c136f12
LeguoTv <luis35624141@gmail.com>
2024-10-10 20:07:01 -0300

    Nova Modificação

--- pagina1.php
+++ pagina1.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "bancodedados";
+$dbname = "sistema_login";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
