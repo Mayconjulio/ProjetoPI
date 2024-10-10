96bf8f4bc476795bc435b167f27859292881ac9f
m4yc0n007 <jabasplay34@gmail.com>
2024-10-10 14:58:51 -0300

    novo banco de dados das paginas de login e cadastro

---
+++ banco de dados/sistema_login.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE sistema_login;  
+  
+USE sistema_login;  
+
+
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(255) NOT NULL
+)
