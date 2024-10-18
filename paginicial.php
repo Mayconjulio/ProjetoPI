83f5ab3d2f1caef04f5905dd9445540bf5ba6d86
m4yc0n007 <jabasplay34@gmail.com>
2024-10-18 12:48:53 -0300

    novos arquivos

--- paginicial.php
+++ paginicial.php
@@ -46,7 +46,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Escolha: Login ou Cadastro</title>
+    <title>LM R. Finacias</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <style>
         * {
